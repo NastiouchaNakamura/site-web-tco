9cda813c6565b8760fac5f9c7ed7a6d2cbcfa7b7
Anaël <anael@barodine.fr>
2025-06-03 23:01:51 +0200

    feat: Remise en dossier.

--- includes/header.php
+++ includes/header.php
@@ -11,11 +11,19 @@
     <div id="curtain" onclick="quit_nav();"></div>
     <nav id="navigation">
         <h1>Tutorat des Carabins d'Orléans</h1>
+        <?php if(isset($_SESSION["adh"])): ?>
+        <div class="user">
+            <p><?= $_SESSION["adh"]->prenom ?> <?= $_SESSION["adh"]->nom ?></p>
+            <a href="/deconnexion"><img src="/icons/key.svg" class="red" alt="[key]">Déconnexion</a>
+        </div>
+        <?php else: ?>
+        <div class="user">
+            <a href="/connexion"><img src="/icons/key.svg" class="green" alt="[key]">Connexion</a>
+        </div>
+        <?php endif ?>
         <a href="/">Accueil</a>
         <a href="/">Informations</a>
         <a href="/espace-adherent"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Tableau de bord</a>
         <a href="/espace-referent"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Espace référent</a>
-        <?php if(isset($_SESSION["adh"])): ?><a href="/deconnexion"><img src="/icons/key.svg" class="red" alt="[key]">Déconnexion</a>
-        <?php else: ?><a href="/connexion"><img src="/icons/key.svg" class="green" alt="[key]">Connexion</a><?php endif ?>
     </nav>
 </header>

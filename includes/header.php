bbf021b3e4168d3da31f1d5a68b0eaa43229cbd0
Anaël <anael@barodine.fr>
2025-06-03 23:52:58 +0200

    feat: Affichage uniquement des pages visibles.

--- includes/header.php
+++ includes/header.php
@@ -12,18 +12,23 @@
     <nav id="navigation">
         <h1>Tutorat des Carabins d'Orléans</h1>
         <?php if(isset($_SESSION["adh"])): ?>
-        <div class="user">
-            <p><?= $_SESSION["adh"]->prenom ?> <?= $_SESSION["adh"]->nom ?></p>
-            <a class="red" href="/deconnexion"><img src="/icons/key.svg" class="white" alt="[key]">Déconnexion</a>
-        </div>
+            <div class="user">
+                <p><?= $_SESSION["adh"]->prenom ?> <?= $_SESSION["adh"]->nom ?></p>
+                <a class="red" href="/deconnexion"><img src="/icons/key.svg" class="white" alt="[key]">Déconnexion</a>
+            </div>
+            <a href="/">Accueil</a>
+            <a href="/">Informations</a>
+            <a href="/espace-adherent"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace adhérent</a>
+            <?php if($_SESSION["adh"]->referent): ?><a href="/espace-referent"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace référent</a><?php endif ?>
+            <?php if($_SESSION["adh"]->bureau): ?><a href="/espace-bureau"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace bureau</a><?php endif ?>
+            <?php if($_SESSION["adh"]->superadmin): ?><a href="/espace-superadmin"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace super-admin</a><?php endif ?>
         <?php else: ?>
-        <div class="user">
-            <a class="green" href="/connexion"><img src="/icons/key.svg" class="white" alt="[key]">Connexion</a>
-        </div>
+            <div class="user">
+                <a class="green" href="/connexion"><img src="/icons/key.svg" class="white" alt="[key]">Connexion</a>
+            </div>
+            <a href="/">Accueil</a>
+            <a href="/">Informations</a>
+            <a href="/espace-adherent"><img src="/icons/lock.svg" class="red" alt="[lock]">Espace adhérent</a>
         <?php endif ?>
-        <a href="/">Accueil</a>
-        <a href="/">Informations</a>
-        <a href="/espace-adherent"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Tableau de bord</a>
-        <a href="/espace-referent"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Espace référent</a>
     </nav>
 </header>

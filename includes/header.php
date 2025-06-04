7305f49946da54fb77cadedd297f648e3779de20
Anaël <anael@barodine.fr>
2025-06-04 14:19:44 +0200

    feat: menu de navigation final

--- includes/header.php
+++ includes/header.php
@@ -29,25 +29,31 @@
             </div>
             <a href="/">Accueil</a>
             <a href="/">Informations</a>
-            <a href="/adherent"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace adhérent</a>
-            <?php if($_SESSION["adh"]->referent): ?><a href="/referent"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace référent</a><?php endif ?>
-            <?php if($_SESSION["adh"]->bureau): ?><a href="/bureau"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace bureau</a><?php endif ?>
-            <?php if($_SESSION["adh"]->superadmin): ?><a href="/superadmin"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace super-admin</a><?php endif ?>
+            <a href="/ressources"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Mes ressources</a>
+            <?php if($_SESSION["adh"]->referent): ?><a href="/ressources/gestion.php"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Gestion des ressources</a><?php endif ?>
+            <a href="/qcm"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Mes QCM</a>
+            <?php if($_SESSION["adh"]->referent): ?><a href="/qcm/gestion.php"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Gestion des QCM</a><?php endif ?>
+            <a href="/forum"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Forum</a>
+            <?php if($_SESSION["adh"]->bureau): ?><a href="/membres"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Gestion des membres</a><?php endif ?>
         <?php else: ?>
             <div class="user">
                 <a class="green" href="/connexion"><img src="/icons/key.svg" class="white" alt="[key]">Se connecter</a>
             </div>
             <a href="/">Accueil</a>
             <a href="/">Informations</a>
-            <a href="/adherent"><img src="/icons/lock.svg" class="red" alt="[lock]">Espace adhérent</a>
+            <a href="/ressources"><img src="/icons/lock.svg" class="red" alt="[lock]">Mes ressources</a>
+            <a href="/qcm"><img src="/icons/lock.svg" class="red" alt="[lock]">Mes QCM</a>
+            <a href="/forum"><img src="/icons/lock.svg" class="red" alt="[lock]">Forum</a>
         <?php endif ?>
     </nav>
 </header>
 <nav class="big-nav" id="navigation">
     <a href="/">Accueil</a>
     <a href="/">Informations</a>
-    <a href="/adherent"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Espace adhérent</a>
-    <?php if(isset($_SESSION["adh"]) && $_SESSION["adh"]->referent): ?><a href="/referent"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace référent</a><?php endif ?>
-    <?php if(isset($_SESSION["adh"]) && $_SESSION["adh"]->bureau): ?><a href="/bureau"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace bureau</a><?php endif ?>
-    <?php if(isset($_SESSION["adh"]) && $_SESSION["adh"]->superadmin): ?><a href="/superadmin"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Espace super-admin</a><?php endif ?>
+    <a href="/ressources"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Mes ressources</a>
+    <?php if(isset($_SESSION["adh"]) && $_SESSION["adh"]->referent): ?><a href="/ressources/gestion.php"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Gestion des ressources</a><?php endif ?>
+    <a href="/qcm"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Mes QCM</a>
+    <?php if(isset($_SESSION["adh"]) && $_SESSION["adh"]->referent): ?><a href="/qcm/gestion.php"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Gestion des QCM</a><?php endif ?>
+    <a href="/forum"><?php if(isset($_SESSION["adh"])): ?><img src="/icons/unlock.svg" class="green" alt="[unlock]"><?php else: ?><img src="/icons/lock.svg" class="red" alt="[lock]"><?php endif ?>Forum</a>
+    <?php if(isset($_SESSION["adh"]) && $_SESSION["adh"]->bureau): ?><a href="/membres/"><img src="/icons/unlock.svg" class="green" alt="[unlock]">Gestion des membres</a><?php endif ?>
 </nav>
